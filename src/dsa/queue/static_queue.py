d6dee9e1f43998970d393c988524402290a7e3b5
adil <adil@exactspace.co>
2024-01-12 18:06:53 +0530

    enque and len  test cases and implementation done

--- src/dsa/queue/static_queue.py
+++ src/dsa/queue/static_queue.py
@@ -5,4 +5,24 @@ class FixedSizeQueue:
         self.__front = -1
 
     def empty(self):
-        return True    
+        return self.__rear and self.__front == -1 
+    
+    def enqueue(self, data):
+        if self.empty():
+            self.__rear+=1
+            self.__front+=1
+            self.__data[self.__front] = data
+        else:    
+            self.__rear = self.__rear + 1
+            self.__data[self.__rear] = data
+
+    def dequeue(self):
+        pass
+        
+   
+    def __str__(self) -> str:
+        temp = ""
+        for index in range(self.__rear + 1):
+            temp = f"{temp}{self.__data[index]}, "
+            index+=1
+        return f"[{temp[:-2]}]"    
--- test/dsa/queue/test_static_queue.py
+++ test/dsa/queue/test_static_queue.py
@@ -3,7 +3,7 @@ from dsa.queue.static_queue import FixedSizeQueue
 
 class TestStaticQueue(unittest.TestCase):
     def setUp(self) -> None:
-        self.queue = FixedSizeQueue(5)
+        self.queue = FixedSizeQueue(4)
         return super().setUp()
     
     def tearDown(self) -> None:
@@ -12,7 +12,34 @@ class TestStaticQueue(unittest.TestCase):
     def test_queue_is_empty_on_creation(self):
         self.assertTrue(self.queue.empty())
 
-    def test_    
+    def test_queue_enque_operation(self):
+        self.queue.enqueue(3)
+        self.assertEqual("[3]", str(self.queue))
+        self.queue.enqueue(6)
+        self.assertEqual("[3, 6]", str(self.queue))
+        self.queue.enqueue(9)
+        self.assertEqual("[3, 6, 9]", str(self.queue))
+        self.queue.enqueue(10)
+        self.assertEqual("[3, 6, 9, 10]", str(self.queue))
+        self.assertFalse(self.queue.empty())
+
+    def test_queue_dequeue_operation(self):
+        self.queue.enqueue(8)
+        self.queue.enqueue(9)
+        self.queue.enqueue(11)
+
+        dequeued_element = self.queue.dequeue()
+        self.assertEqual("[9, 11]", str(self.queue))
+
+        dequeued_element = self.queue.dequeue()
+        self.assertEqual("[11]", str(self.queue))
+
+        dequeued_element = self.queue.dequeue()
+        self.assertEqual("[]", str(self.queue))
+
+
+
+
 
 
 if __name__ == "__main__":
